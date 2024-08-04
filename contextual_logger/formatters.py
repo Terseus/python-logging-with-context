b20881df01c7a526e783d4cf2a23c24c940b5341
Terseus <terseus@fastmail.com>
2024-08-04 17:26:36 +0200

    feat: add serializers to ExtraTextFormatter

--- contextual_logger/formatters.py
+++ contextual_logger/formatters.py
@@ -1,5 +1,21 @@
+from collections import defaultdict
 from logging import Formatter, LogRecord
-from typing import Set
+from typing import Any, Callable, Mapping, Optional, Set, Type, TypeVar
+
+T = TypeVar("T")
+ValueSerializerMap = Mapping[Type[T], Callable[[T], str]]
+
+
+def _str_serializer(value: str) -> str:
+    return f'"{value}"'
+
+
+def _none_serializer(_: None) -> str:
+    return "<null>"
+
+
+def _float_serializer(value: float) -> str:
+    return f"{value:.5f}"
 
 
 class ExtraTextFormatter(Formatter):
@@ -28,6 +44,28 @@ class ExtraTextFormatter(Formatter):
         "thread",
         "threadName",
     }
+    _DEFAULT_SERIALIZERS: ValueSerializerMap = {
+        str: _str_serializer,
+        type(None): _none_serializer,
+        float: _float_serializer,
+    }
+
+    def __init__(
+        self,
+        *args,
+        serializers: Optional[ValueSerializerMap] = None,
+        default_serializer: Callable[[Any], str] = str,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        if serializers is None:
+            serializers = {}
+        serializers = self._DEFAULT_SERIALIZERS | serializers  # type: ignore
+        self._serializers = defaultdict(lambda: default_serializer) | serializers  # type: ignore
+
+    def _serialize_value(self, value: Any) -> str:
+        serializer = self._serializers[type(value)]
+        return serializer(value)
 
     def format(self, record: LogRecord) -> str:
         extras = set(record.__dict__.keys()) - self._LOG_RECORD_ATTRIBUTES
@@ -35,5 +73,7 @@ class ExtraTextFormatter(Formatter):
         if not extras:
             return message
 
-        extras_msg = ", ".join(f'{name}="{getattr(record, name)}"' for name in extras)
+        extras_msg = ", ".join(
+            f"{name}={self._serialize_value(getattr(record, name))}" for name in sorted(extras)
+        )
         return f"{message}; {extras_msg}"
--- tests/contextual_logger/test_formatters.py
+++ tests/contextual_logger/test_formatters.py
@@ -1,4 +1,6 @@
 import logging
+import math
+from dataclasses import dataclass
 from typing import Callable, Generator
 
 import pytest
@@ -7,6 +9,16 @@ from pytest import LogCaptureFixture
 from contextual_logger.formatters import ExtraTextFormatter
 
 CaplogFactory = Callable[[logging.Formatter], LogCaptureFixture]
+
+
+@dataclass
+class MyValue:
+    name: str
+    value: str
+
+
+def _my_value_serializer(value: MyValue) -> str:
+    return f"MyValue({value.name}={value.value})"
 
 
 @pytest.fixture
@@ -26,6 +38,28 @@ def test_extra_text_formatter_ok(caplog_factory: CaplogFactory):
     caplog = caplog_factory(ExtraTextFormatter(fmt="%(message)s"))
     logger = logging.getLogger(__name__)
     with caplog.at_level(logging.INFO):
-        logger.info("Testing", extra={"key": "value"})
-    expected = 'Testing; key="value"\n'
+        logger.info("Testing", extra={"key1": "value", "key2": 100, "key3": None, "key4": math.pi})
+    expected = 'Testing; key1="value", key2=100, key3=<null>, key4=3.14159\n'
+    assert caplog.text == expected
+
+
+def test_extra_text_formatter_custom_serializer_ok(caplog_factory: CaplogFactory):
+    caplog = caplog_factory(
+        ExtraTextFormatter(fmt="%(message)s", serializers={MyValue: _my_value_serializer})
+    )
+    logger = logging.getLogger(__name__)
+    with caplog.at_level(logging.INFO):
+        logger.info("Testing", extra={"key1": "value1", "key2": MyValue("my_name", "my_value")})
+    expected = 'Testing; key1="value1", key2=MyValue(my_name=my_value)\n'
+    assert caplog.text == expected
+
+
+def test_extra_text_formatter_default_serializer_ok(caplog_factory: CaplogFactory):
+    caplog = caplog_factory(
+        ExtraTextFormatter(fmt="%(message)s", default_serializer=_my_value_serializer)
+    )
+    logger = logging.getLogger(__name__)
+    with caplog.at_level(logging.INFO):
+        logger.info("Testing", extra={"key1": "value1", "key2": MyValue("my_name", "my_value")})
+    expected = 'Testing; key1="value1", key2=MyValue(my_name=my_value)\n'
     assert caplog.text == expected
